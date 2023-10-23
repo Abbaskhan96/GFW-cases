50c5cb0123354160808b155641cca9469a0fae06
QA Abbas <khanabbas000@gmail.com>
2023-10-23 06:36:48 +0500

    second commit

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ import os
 import time
 
 opt= Options()
-opt.add_experimental_option("mobileEmulation", {"deviceName": "iPad"})
+opt.add_experimental_option("mobileEmulation", {"deviceName": "Pixel 2"})
 opt.add_experimental_option("detach",True)
 token=os.getenv("PERCY_TOKEN")
 if token:
     print("Token is :", token)
 driver= webdriver.Chrome(options= opt)
 driver.maximize_window()
-driver.get("https://gardenforwildlife.com/")
-time.sleep(10)
+driver.get("https://facebook.com/")
+time.sleep(8)
 
 #percy_snapshot(driver, "HomePage")
 
