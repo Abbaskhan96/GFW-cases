61dfd27597b03e35c8649e6063f05935c04823a3
QA Abbas <khanabbas000@gmail.com>
2023-10-29 06:19:14 +0500

    html-check-1

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 
 #python developer file that he adds code and push in git
 
-abcd
+
 print("This is developer build")
 print("for testing github hooks..")
 print("This is the new developer's commits")
