6ca59b4d7e64a77a14fb7a3a592c446d10aba299
QA Abbas <khanabbas000@gmail.com>
2023-10-30 19:55:12 +0500

    test-env

--- build.py
+++ build.py
@@ -5,4 +5,4 @@
 print("This is developer build")
 print("for testing github hooks..")
 print("This is the new developer's commits")
-print("Testing for slack integration.")
+print("Testing for slack integration...")
